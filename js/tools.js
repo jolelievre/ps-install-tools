4ba1986c9d534a9994269fdb90a85a95606df60b
Jonathan Lelievre <jo.lelievre@gmail.com>
2024-04-26 14:22:40 +0200

    Fix warmup backoffice

--- js/tools.js
+++ js/tools.js
@@ -1,7 +1,7 @@
 module.exports.loginBO = async (page, config) => {
     await page.type('#email', config.email);
     await page.type('#passwd', config.password);
-    await page.click('button[name="submitLogin"]');
-    await page.waitForNavigation({waitUntil: 'domcontentloaded'});
+    await page.click('#submit_login');
+    await page.waitForSelector('body.ps_back-office');
     console.log('Login to BO successful');
 };
