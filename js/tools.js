cfa6f190ae176fb8ddf005158a77a2788bf47dd7
Jonathan Lelievre <jo.lelievre@gmail.com>
2024-06-13 11:33:06 +0200

    WAit for load in initial login is optional because on recent version it is too fast to be detected

--- js/tools.js
+++ js/tools.js
@@ -2,7 +2,11 @@ module.exports.loginBO = async (page, config) => {
     await page.type('#email', config.email);
     await page.type('#passwd', config.password);
     await page.click('#submit_login');
+    try {
+        await page.waitForNavigation({waitUntil: 'load', timeout: 5000});
+    } catch {
+        // Do nothing we only wait for the load for old versions that are slower
+    }
     await page.waitForSelector('body.ps_back-office');
-    await page.waitForNavigation({waitUntil: 'load'});
     console.log('Login to BO successful');
 };
